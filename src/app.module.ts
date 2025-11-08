89edcbf84791ddb852f5bf72ac764e4ecc62d302
Lyriom <130936689+Lyriom@users.noreply.github.com>
2025-11-08 18:07:27 -0500

    Add root endpoint for health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
-import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import { ReservasModule } from './reservas/reservas.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    DatabaseModule,
-    AuthModule,
-    UsersModule,
-    ReservasModule,
-  ],
+  imports: [UsersModule, AuthModule, ReservasModule, DatabaseModule],
+  controllers: [AppController],
 })
 export class AppModule {}
-
