d14b767cde12f679b4aa08570394fedf3c1b09d8
Lyriom <130936689+Lyriom@users.noreply.github.com>
2025-11-12 18:58:19 -0500

    Agrega móduloo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ReservasModule } from './reservas/reservas.module';
 import { DatabaseModule } from './database/database.module';
+
 import { TiposHabitacionModule } from './tipos-habitacion/tipos-habitacion.module';
 import { EstanciasModule } from './estancias/estancias.module';
 import { EstanciaHabitacionModule } from './estancia-habitacion/estancia-habitacion.module';
+
 import { ClientesModule } from './clientes/clientes.module';
+import { RolesModule } from './roles/roles.module';
+
+import { FacturasModule } from './facturas/facturas.module';
 import { DetalleFacturaModule } from './detalle-factura/detalle-factura.module';
-
-
-
+import { PagosModule } from './pagos/pagos.module';
 
 @Module({
   imports: [
-    UsersModule,
-    AuthModule,
-    ReservasModule,
     DatabaseModule,
+
+    AuthModule,
+    UsersModule,
+
+    ClientesModule,
+    RolesModule,
+
     TiposHabitacionModule,
     EstanciasModule,
     EstanciaHabitacionModule,
-    ClientesModule,
-    DetalleFacturaModule,
 
+    ReservasModule,
+    FacturasModule,
+    DetalleFacturaModule,
+    PagosModule,
   ],
   controllers: [AppController],
 })
