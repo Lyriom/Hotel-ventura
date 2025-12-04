fd96ebb11cbbb8da39ee10b9970269836bbc7137
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2025-12-04 12:36:24 -0500

    Refactor bootstrap function for clarity and efficiency
    Added custom JSON serialization for BigInt and removed duplicate CORS and ValidationPipe configurations.

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  (BigInt.prototype as any).toJSON = function () {
+    return Number(this);
+  };
+  app.enableCors(); 
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true,
       transform: true,
     }),
   );
-  
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-
-
-  app.enableCors();
 
   await app.listen(3000);
-  console.log('API Hotel Ventura corriendo en http://localhost:3000');
 }
 bootstrap();
